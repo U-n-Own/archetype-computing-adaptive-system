8696bd5eba4d74323ee190f3dd470a1aa5c2c758
U-n-Own <cenzogargano@gmail.com>
2025-11-10 17:46:46 +0100

    change back to old test version

--- experiments/smnist.py
+++ experiments/smnist.py
@@ -108,10 +108,8 @@ def test(data_loader, classifier, scaler):
         images = images.to(device)
         images = images.view(images.shape[0], -1).unsqueeze(-1)
         output = model(images)[-1][0]
-        if isinstance(output, list):
-            output = output[0]
-    activations.append(output.cpu())
-    ys.append(labels)
+        activations.append(output.cpu())
+        ys.append(labels)
     activations = torch.cat(activations, dim=0).numpy()
     activations = scaler.transform(activations)
     ys = torch.cat(ys, dim=0).numpy()
